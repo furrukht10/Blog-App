637bdf16e3cff7d0d0d33b34e3926f870abd6e9b
furrukht10 <furrukht10@gmail.com>
2022-01-10 13:49:56 -0500

    revamp

--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,14 @@
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require("mongoose");
-const User = require("../models/User");
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const User = require('../models/User')
 
 module.exports = function (passport) {
-  const id =
-    "326712317858-frkvhq3jbh4o6douo37gfj86buf6hg6v.apps.googleusercontent.com";
-  const secret = "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";
   passport.use(
     new GoogleStrategy(
       {
-        clientID: id,
-        clientSecret: secret,
-        callbackURL: "/auth/google/callback",
-        proxy: true,
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
@@ -21,29 +17,29 @@ module.exports = function (passport) {
           firstName: profile.name.givenName,
           lastName: profile.name.familyName,
           image: profile.photos[0].value,
-        };
+        }
 
         try {
-          let user = await User.findOne({ googleId: profile.id });
+          let user = await User.findOne({ googleId: profile.id })
 
           if (user) {
-            done(null, user);
+            done(null, user)
           } else {
-            user = await User.create(newUser);
-            done(null, user);
+            user = await User.create(newUser)
+            done(null, user)
           }
         } catch (err) {
-          console.error(err);
+          console.error(err)
         }
       }
     )
-  );
+  )
 
   passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+    done(null, user.id)
+  })
 
   passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
-  });
-};
+    User.findById(id, (err, user) => done(err, user))
+  })
+}
