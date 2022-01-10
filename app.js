f4d135950e20d3b38ccba74661587cb79dbcc09f
furrukht10 <furrukht10@gmail.com>
2022-01-10 12:51:56 -0500

    fix bug

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ require("./config/passport")(passport);
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
